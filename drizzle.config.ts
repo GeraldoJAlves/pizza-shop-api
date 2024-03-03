51684b7ca287dcf07956fb7f4ebfc3807907a2f3
geraldojalves <junior.alves.2007@gmail.com>
2024-03-03 19:45:53 -0300

    chore: migrate script

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,9 @@
 import type { Config } from 'drizzle-kit'
 export default {
-  schema: './src/schema/index.ts',
+  schema: './src/db/schema/index.ts',
   out: './drizzle',
   driver: 'pg',
+  dbCredentials: {
+    connectionString: 'postgres://pizza:123456@localhost:5432/pizzashop',
+  },
 } satisfies Config
---
+++ src/db/migrate.ts
@@ -0,0 +1,16 @@
+import { drizzle } from 'drizzle-orm/postgres-js'
+import { migrate } from 'drizzle-orm/postgres-js/migrator'
+import postgres from 'postgres'
+
+const connection = postgres(
+  'postgres://pizza:123456@localhost:5432/pizzashop',
+  { max: 1 },
+)
+
+const db = drizzle(connection)
+
+await migrate(db, { migrationsFolder: 'drizzle' })
+
+await connection.end()
+
+process.exit()
