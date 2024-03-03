2cb16f1d8ec6f98af6d28136a0727a51939ea105
geraldojalves <junior.alves.2007@gmail.com>
2024-03-03 19:04:26 -0300

    chore: restaurants relations

--- src/schema/restaurants.ts
+++ src/schema/restaurants.ts
@@ -1,5 +1,8 @@
 import { createId } from '@paralleldrive/cuid2'
+import { relations } from 'drizzle-orm'
 import { pgTable, text, timestamp } from 'drizzle-orm/pg-core'
+
+import { users } from '.'
 
 export const restaurants = pgTable('restaurants', {
   id: text('id')
@@ -7,6 +10,19 @@ export const restaurants = pgTable('restaurants', {
     .primaryKey(),
   name: text('name').notNull(),
   description: text('description'),
+  managerId: text('manager_id').references(() => users.id, {
+    onDelete: 'set null',
+  }),
   createdAt: timestamp('created_at').notNull().defaultNow(),
   updatedAt: timestamp('updated_at').notNull().defaultNow(),
+})
+
+export const restaurantsRelations = relations(restaurants, ({ one }) => {
+  return {
+    manager: one(users, {
+      fields: [restaurants.managerId],
+      references: [users.id],
+      relationName: 'restaurant_manager',
+    }),
+  }
 })
