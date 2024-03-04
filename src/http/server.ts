0ce8924f96c0d66eb8bedf4c9e7c7dfbf1d37c83
geraldojalves <junior.alves.2007@gmail.com>
2024-03-03 23:39:24 -0300

    feat: add POST /restaurants

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,40 @@
-import { Elysia } from 'elysia'
+import { Elysia, t } from 'elysia'
 
-const app = new Elysia().get('/', () => {
-  return 'pizza-shop'
-})
+import { db } from '../db/connection'
+import { restaurants, users } from '../db/schema'
+
+const app = new Elysia().post(
+  '/restaurants',
+  async ({ body, set }) => {
+    const { restaurantName, managerName, email, phone } = body
+
+    const [manager] = await db
+      .insert(users)
+      .values({
+        name: managerName,
+        email,
+        phone,
+        role: 'manager',
+      })
+      .returning({ id: users.id })
+
+    await db.insert(restaurants).values({
+      name: restaurantName,
+      managerId: manager.id,
+    })
+
+    set.status = 'Created'
+  },
+  {
+    body: t.Object({
+      restaurantName: t.String(),
+      managerName: t.String(),
+      email: t.String({ format: 'email' }),
+      phone: t.String(),
+    }),
+  },
+)
 
 app.listen(3333, () => {
-  console.log('Server started on http://localhost::3333')
+  console.log('🔥 Server started on http://localhost:3333')
 })
