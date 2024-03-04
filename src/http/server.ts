be4737eab413ffd3546f372530eba5d3bffce874
geraldojalves <junior.alves.2007@gmail.com>
2024-03-04 09:10:49 -0300

    refactor: separate routes

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,39 +1,8 @@
-import { Elysia, t } from 'elysia'
+import { Elysia } from 'elysia'
 
-import { db } from '../db/connection'
-import { restaurants, users } from '../db/schema'
+import { registerRestaurant } from './routes/register-restaurant'
 
-const app = new Elysia().post(
-  '/restaurants',
-  async ({ body, set }) => {
-    const { restaurantName, managerName, email, phone } = body
-
-    const [manager] = await db
-      .insert(users)
-      .values({
-        name: managerName,
-        email,
-        phone,
-        role: 'manager',
-      })
-      .returning({ id: users.id })
-
-    await db.insert(restaurants).values({
-      name: restaurantName,
-      managerId: manager.id,
-    })
-
-    set.status = 'Created'
-  },
-  {
-    body: t.Object({
-      restaurantName: t.String(),
-      managerName: t.String(),
-      email: t.String({ format: 'email' }),
-      phone: t.String(),
-    }),
-  },
-)
+const app = new Elysia().use(registerRestaurant)
 
 app.listen(3333, () => {
   console.log('🔥 Server started on http://localhost:3333')
