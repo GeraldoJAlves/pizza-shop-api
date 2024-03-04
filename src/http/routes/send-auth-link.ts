e1767e9fe99edd704d88f70207ec81e83a245476
geraldojalves <junior.alves.2007@gmail.com>
2024-03-04 09:39:18 -0300

    feat: generate auth link

---
+++ src/http/routes/send-auth-link.ts
@@ -0,0 +1,43 @@
+import { createId } from '@paralleldrive/cuid2'
+import { Elysia, t } from 'elysia'
+
+import { db } from '../../db/connection'
+import { authLinks } from '../../db/schema'
+import { env } from '../../env'
+
+export const sendAuthLink = new Elysia().post(
+  '/auth-links',
+  async ({ body, set }) => {
+    const { email } = body
+
+    const user = await db.query.users.findFirst({
+      where(fields, { eq }) {
+        return eq(fields.email, email)
+      },
+    })
+
+    set.status = 'No Content'
+    if (!user) {
+      return
+    }
+
+    const authLinkCode = createId()
+
+    await db.insert(authLinks).values({
+      code: authLinkCode,
+      userId: user.id,
+    })
+
+    const url = new URL('/auth-links/authenticate', env.API_BASE_URL)
+
+    url.searchParams.set('code', authLinkCode)
+    url.searchParams.set('redirect', env.AUTH_REDIRECT_URL)
+
+    console.log('auth link', url.toString())
+  },
+  {
+    body: t.Object({
+      email: t.String({ format: 'email' }),
+    }),
+  },
+)
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,9 @@
 import { Elysia } from 'elysia'
 
 import { registerRestaurant } from './routes/register-restaurant'
+import { sendAuthLink } from './routes/send-auth-link'
 
-const app = new Elysia().use(registerRestaurant)
+const app = new Elysia().use(registerRestaurant).use(sendAuthLink)
 
 app.listen(3333, () => {
   console.log('🔥 Server started on http://localhost:3333')
