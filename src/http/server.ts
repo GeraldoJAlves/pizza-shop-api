5062998967937ed9729895de5d095f1a739a85f1
geraldojalves <junior.alves.2007@gmail.com>
2024-03-09 15:30:01 -0300

    feat: dispatch order

--- src/http/server.ts
+++ src/http/server.ts
@@ -5,6 +5,7 @@ import { approveOrder } from './routes/approve-order'
 import { authenticateFromLink } from './routes/authenticate-from-link'
 import { cancelOrder } from './routes/cancel-order'
 import { deliverOrder } from './routes/deliver-order'
+import { dispatchOrder } from './routes/dispatch-order'
 import { getOrderDetails } from './routes/get-order-details'
 import { getOrders } from './routes/get-orders'
 import { getProfile } from './routes/get-profile'
@@ -25,6 +26,7 @@ const app = new Elysia()
   .use(approveOrder)
   .use(cancelOrder)
   .use(deliverOrder)
+  .use(dispatchOrder)
   .onError(({ code, error, set }) => {
     switch (code) {
       case 'VALIDATION': {
